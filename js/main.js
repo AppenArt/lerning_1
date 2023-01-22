ae8ed2f0705f678615fb4ef39890a9c53347d612
Grzegorz Błażek <biuro@appenart.pl>
2023-01-22 21:31:33 +0100

    add section dream

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const handleNavItemsAnimation = () => {
 	allNavItems.forEach((item) => {
 		item.classList.toggle("nav-items-animation");
 		item.style.animationDelay = "." + delayTime + "s";
-        delayTime++
+		delayTime++;
 	});
 };
 
